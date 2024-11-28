40e8f6f0ca78ea846831a286e8eed255748785af
WrathLore <sarah.turner.2001@icloud.com>
2024-11-27 23:48:35 -0600

    adjusted spatial noise of bush rustling noise

--- Class Project/Assets/Scripts/VariousSounds.cs
+++ Class Project/Assets/Scripts/VariousSounds.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class VariousSounds : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip audioClip;
     //use for various sounds that only want to play on objects otherwise not interacted with
     //ie objects that don't have unique scripts already
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player") && audioSource != null)
+        if(other.CompareTag("Player") && audioSource != null && audioClip != null)
         {
-            audioSource.Play();
+            audioSource.PlayOneShot(audioClip);
         }
     }
 }
